cb96bc64762acfcee3b0090dae6e188935f52d2d
samuelebruni <samuele_bruni@hotmail.com>
2023-09-27 17:11:46 +0200

    add working search bar in the SearchArchetype.vue

--- src/components/SearchArchetype.vue
+++ src/components/SearchArchetype.vue
@@ -15,7 +15,7 @@ export default {
 </script>
 
 <template>
-    <select class="border-0 rounded-2 p-1 shadow" :v-model="store.nameArchetype" @change="$emit('searching')">
+    <select class="border-0 rounded-2 p-1 shadow" v-model="store.nameArchetype" @change="$emit('searching')">
         <option value="" disabled selected>Seleziona un archetipo..</option>
         <option v-for="archetype in store.archetypeList" :value="archetype.archetype_name">{{ archetype.archetype_name
         }}
